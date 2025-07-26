946a545002a824cbfb917615169db9c5b9db9d3c
Tan Yi Peng <61421451+tanyipeng834@users.noreply.github.com>
2025-07-26 23:59:19 +0800

    workiing buddy allocation

--- src/free_list.c
+++ src/free_list.c
@@ -161,7 +161,7 @@ int buddy_init()
         // this is the starting address of the 
         buddy_head = (block_header_t*)(head);
         memory_base_size = head->size + sizeof(node_t);
-        buddy_head->is_free =0;
+        buddy_head->is_free =1;
         return 0;
     
     
@@ -174,31 +174,6 @@ int buddy_init()
 
 }
 
-// void * buddy_allocate(int memory_size)
-// {
-    
-//         size_t total_memory_request = sizeof(block_header_t) +((memory_size/8)+1)*8;
-//         size_t max_power = log2(memory_base_size);
-        
-//         int memory_request_power = get_lowest_block_size(memory_base_size,total_memory_request,max_power);
-//         printf("The lowest power that I have is:%d",memory_request_power);
-//         size_t block_size = pow(2, memory_request_power);
-//         char * walk = (char *)buddy_head;
-//         // Iterate until the last possible block
-//         char* walk_end = walk +memory_base_size - block_size;
-//        while (walk <= walk_end) {
-//         block_header_t *header = (block_header_t *)walk;
-//         if (header->is_free != 1) {
-//         return (void *)(header + 1);
-//     }
-
-
-            
-//             char * walk  = ((char*)(walk)) +block_size;
-
-//         }
-        
-//     }
 
     
 
@@ -232,11 +207,13 @@ void * buddy_allocation(int memory_size,block_header_t* current_block)
    
 
     size_t required_size = memory_size + sizeof(block_header_t);
+
+    
     
 
 
     
-    if(!current_block || current_block->is_free){
+    if(!current_block || !current_block->is_free){
         return NULL;
     }
 
@@ -245,7 +222,7 @@ void * buddy_allocation(int memory_size,block_header_t* current_block)
     }
 
     if(current_block->size<required_size*2){
-        current_block->is_free =1;
+        current_block->is_free =0;
         printf("current block%p",current_block );
         return (void*) (current_block+1);
 
@@ -258,10 +235,10 @@ void * buddy_allocation(int memory_size,block_header_t* current_block)
     block_header_t * right_child = (block_header_t*)((char *)current_block + half_size);
 
     left_child->size = half_size;
-    left_child->is_free = 0;
+    left_child->is_free = 1;
 
     right_child->size = half_size;
-    right_child->is_free = 0;
+    right_child->is_free = 1;
 
     
     void* result = buddy_allocation(memory_size, left_child);
@@ -274,6 +251,38 @@ void * buddy_allocation(int memory_size,block_header_t* current_block)
     
 
 
+
+}
+
+int buddy_free(void* ptr){
+
+
+    if(ptr !=NULL)
+    {
+       block_header_t* block = ((block_header_t*)ptr) - 1;
+        size_t block_size = block->size;
+        int bit_mask = (1 << (int)log2(block_size)); 
+        uintptr_t block_addr = (uintptr_t)block;
+        uintptr_t buddy_addr = block_addr ^ bit_mask;
+
+        block_header_t* buddy = (block_header_t*)buddy_addr;
+
+
+        if(buddy !=NULL && buddy->is_free)
+        {
+            block = (block_addr<buddy_addr)? block:buddy;
+            block->is_free =1;
+            block->size = block_size*=2;
+            return 0;
+
+        }
+
+
+
+
+    }
+
+    return -1;
 
 }
 
